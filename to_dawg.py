b84bd737972b7d454f72dd2bc4d185e474352f40
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2014-07-23 17:45:30 -0400

    store string sets as tries

---
+++ to_dawg.py
@@ -0,0 +1,33 @@
+import sys
+
+import os 
+from os import makedirs, listdir
+from os.path import join, exists
+from dawg import DAWG
+
+if __name__ == '__main__':
+	assert len(sys.argv) == 2
+	source_dir = sys.argv[1]
+	if source_dir.endswith("/"):
+		source_dir = source_dir[:-1]
+	assert exists(source_dir)
+        target_dir = source_dir + "_dawg"
+	if exists(target_dir):
+		os.rmdir(target_dir)
+	
+	makedirs(target_dir)
+	source_files = listdir(source_dir)
+
+
+	for filename in source_files:
+		print filename 
+		with open(join(source_dir, filename), 'r') as input_file:
+			contents = input_file.read()
+			with open(join(target_dir, filename + ".dawg"), 'w') as output_file:
+				if filename == 'mappings':
+					# copy source to destination 
+					output_file.write(contents)
+				else:
+					lines = contents.split("\n")
+					d = DAWG(l for l in lines if len(l) > 0)
+					d.write(output_file)
